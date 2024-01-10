7be568b9ab93d3a16d34938c734b81915f8cdffe
Mohammad Alhussan <malhussan@meshcloud.io>
2024-01-10 17:15:59 +0100

    test timeout (actual)

--- dependency/main.tf
+++ dependency/main.tf
@@ -13,10 +13,22 @@ resource "local_file" "dependency" {
   filename = "${path.module}/${var.filename}-dependency.txt"
 }
 
+resource "time_sleep" "wait_70_minutes" {
+  depends_on = [local_file.dependency]
+
+  create_duration = "70m"
+}
+
+resource "local_file" "dependency_next" {
+  content    = "${var.content}-next"
+  filename   = "${path.module}/${var.filename}-dependency-next.txt"
+  depends_on = [time_sleep.wait_70_minutes]
+}
+
 output "file" {
   value = {
-    note            = "This is a test branch"
     filename        = local_file.dependency.filename
     file_permissons = local_file.dependency.file_permission
+    filename2       = local_file.dependency_next.filename
   }
 }
