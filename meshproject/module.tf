83fd6ba87afef5d38060432f862fde94d3893660
Stefan Tomm <stomm@meshcloud.io>
2026-01-26 09:17:35 +0100

    fix payment method name in meshproject module

--- meshproject/module.tf
+++ meshproject/module.tf
@@ -26,7 +26,7 @@ resource "meshstack_project" "example" {
     owned_by_workspace = var.workspace_identifier
   }
   spec = {
-    payment_method_identifier = "my-payment-method-identifier"
+    payment_method_identifier = "default"
     display_name              = "My Project ${var.project_identifier}"
     tags = {
     }
